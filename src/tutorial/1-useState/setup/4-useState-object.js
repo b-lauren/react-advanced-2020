9ed2069522a4eb416f7ab73cd964beeb4077aff5
Rebecca Ross <b.laurenross@gmail.com>
2021-02-14 19:44:40 +0000

    more use state

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
   return <h2>useState object example</h2>;
 };
 
